b62fe14395c1a987e4bc7ead09d9cb5a5708d4df
FilkaG <103595000+FilkaG@users.noreply.github.com>
2022-09-15 23:09:46 +0300

    Файл программы
    Программа для решения поставленной задачи на С#

---
+++ Work.cs
@@ -0,0 +1,27 @@
+﻿string[] MassOfString = {"hello","2","my name","Hi","123","Alluminium minimum","-22"};
+
+void PrintArray (string[] Mass)
+{
+    for(int i=0; i<Mass.GetLength(0); i++)
+    {
+        Console.Write($"{Mass[i]} ");
+    }
+}
+
+string[] StrLength (string[] Massiv, int n)
+{
+    string[] Str = new string[Massiv.GetLength(0)];
+    int j=0;
+    for(int i=0; i<MassOfString.GetLength(0); i++)
+    {
+        if (MassOfString[i].Length<=n)
+        {
+            Str[j]=MassOfString[i];
+            j++;
+        }
+    }
+    return Str;
+}
+
+string[] Answer = StrLength(MassOfString,3);
+PrintArray(Answer);
